824d1ecd04166343f69b111add0b9aac13d8294e
Andrew Shoats <andrew.shoats.sw@gmail.com>
2022-12-29 21:50:15 -0500

    Some more tries to animate.

--- animate.py
+++ animate.py
@@ -2,31 +2,33 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+def animation_setup(ball):
+    circle = plt.Circle((ball._pos[0][0],ball._pos[0][1]),ball._radius)
 
-def setup_animation(fig):
-    
-    ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
-    ball_pos = plt.Circle((ball._pos[0][0],ball._pos[0][1]),ball._radius)
-    ax.add_patch(ball_pos)
+    return circle,
 
-    return ball_pos,
+def init():
 
+    return circle,
 
-def animate(i, ball_pos):
+def animate(i):
 
     y = ball._pos[i][1]
     x = ball._pos[i][0]
 
-    ball_pos.center = (x,y)
+    circle.center = (x,y)
 
-    return ball_pos,
+    return circle,
 
 def run_animation(ball):
-    
-    fig = plt.figure("Ball World")
-    
-    ani = animation.FuncAnimation(
-        fig, animate, init_func=setup_animation,frames=360, interval=20,blit=True)
+
+    fig = plt.figure() 
+    circle = animation_setup(ball)
+
+    ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
+    ax.add_patch(circle)
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init,frames=360, interval=20,blit=True)
     plt.show()
 
     return
